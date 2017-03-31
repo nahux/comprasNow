e376bc4b5eab194541c78d58d9ff5166020da35b
Nahuel Aparicio <nahuel94@gmail.com>
2017-03-31 13:18:10 -0300

    Inicializado proyecto

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/comprasnow/admin/');
+define('HTTP_CATALOG', 'http://localhost/comprasnow/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/comprasnow/admin/');
+define('HTTPS_CATALOG', 'http://localhost/comprasnow/');
+
+// DIR
+define('DIR_APPLICATION', 'D:/xampp/htdocs/comprasnow/admin/');
+define('DIR_SYSTEM', 'D:/xampp/htdocs/comprasnow/system/');
+define('DIR_IMAGE', 'D:/xampp/htdocs/comprasnow/image/');
+define('DIR_LANGUAGE', 'D:/xampp/htdocs/comprasnow/admin/language/');
+define('DIR_TEMPLATE', 'D:/xampp/htdocs/comprasnow/admin/view/template/');
+define('DIR_CONFIG', 'D:/xampp/htdocs/comprasnow/system/config/');
+define('DIR_CACHE', 'D:/xampp/htdocs/comprasnow/system/storage/cache/');
+define('DIR_DOWNLOAD', 'D:/xampp/htdocs/comprasnow/system/storage/download/');
+define('DIR_LOGS', 'D:/xampp/htdocs/comprasnow/system/storage/logs/');
+define('DIR_MODIFICATION', 'D:/xampp/htdocs/comprasnow/system/storage/modification/');
+define('DIR_UPLOAD', 'D:/xampp/htdocs/comprasnow/system/storage/upload/');
+define('DIR_CATALOG', 'D:/xampp/htdocs/comprasnow/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'comprasnow');
+define('DB_PASSWORD', 'admin');
+define('DB_DATABASE', 'comprasnow');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/comprasnow/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/comprasnow/');
+
+// DIR
+define('DIR_APPLICATION', 'D:/xampp/htdocs/comprasnow/catalog/');
+define('DIR_SYSTEM', 'D:/xampp/htdocs/comprasnow/system/');
+define('DIR_IMAGE', 'D:/xampp/htdocs/comprasnow/image/');
+define('DIR_LANGUAGE', 'D:/xampp/htdocs/comprasnow/catalog/language/');
+define('DIR_TEMPLATE', 'D:/xampp/htdocs/comprasnow/catalog/view/theme/');
+define('DIR_CONFIG', 'D:/xampp/htdocs/comprasnow/system/config/');
+define('DIR_CACHE', 'D:/xampp/htdocs/comprasnow/system/storage/cache/');
+define('DIR_DOWNLOAD', 'D:/xampp/htdocs/comprasnow/system/storage/download/');
+define('DIR_LOGS', 'D:/xampp/htdocs/comprasnow/system/storage/logs/');
+define('DIR_MODIFICATION', 'D:/xampp/htdocs/comprasnow/system/storage/modification/');
+define('DIR_UPLOAD', 'D:/xampp/htdocs/comprasnow/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'comprasnow');
+define('DB_PASSWORD', 'admin');
+define('DB_DATABASE', 'comprasnow');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
